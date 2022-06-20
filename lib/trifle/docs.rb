f1f85f97364f7a9bf3a061692ba7524ec4008830
Jozef Vaclavik <jozef@hey.com>
2022-06-20 07:37:16 +0200

    feat: add initial gem setup

---
+++ lib/trifle/docs.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "docs/version"
+
+module Trifle
+  module Docs
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/trifle/docs_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Trifle::Docs do
+  it "has a version number" do
+    expect(Trifle::Docs::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
